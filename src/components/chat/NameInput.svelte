499547708873d882179bc21b33824e198c25a9d9
Bohdan <bohdanimiela@gmail.com>
2020-02-10 15:37:20 +0100

    Refactor NameInput
    - block adding user when submitting empty input
    - display info if uesr name is already in use

--- src/components/chat/NameInput.svelte
+++ src/components/chat/NameInput.svelte
@@ -9,23 +9,32 @@
   activeUsers.subscribe(v => (usersOnline = v));
 
   let nameInput = "";
+  let info = ""
 
   function saveName(e) {
     for (const user of usersOnline) {
       if (user.name === nameInput) {
-        alert(`${user.name} is already chatting. Please choose other name.`);
-        nameInput = "";
+        nameAlreadyInUse(nameInput)
         return;
       }
     }
     name.set(nameInput);
     socket.emit("new user", nameInput);
   }
+
+  function nameAlreadyInUse(name) {
+    info =`${name} is already in use. Please choose other name.`;
+    nameInput = "";
+  }
 </script>
 
 <style>
   section {
     height: 120px;
+  }
+
+  p {
+    text-align: center;
   }
 
   input {
@@ -44,10 +53,12 @@
 
 <section>
   <form on:submit|preventDefault={saveName} out:fade={{ duration: 300 }}>
+    <p>{info}</p>
     <input
       autocomplete="off"
       placeholder="enter your name here..."
       maxlength="16"
+      required
       bind:value={nameInput} />
     <button>let's chat</button>
   </form>
